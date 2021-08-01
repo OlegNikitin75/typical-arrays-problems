ea6cd5fab72c62193c7dc5278b9f67422416a49a
Aleh Nikitsin <nov075@mail.ru>
2021-08-01 14:44:00 +0300

    feat: added a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array == undefined) {
+		return 0
+	} else if (array.length == 0) {
+		return 0
+	} else return Math.min.apply(null, array)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array == undefined) {
+		return 0
+	} else if (array.length == 0) {
+		return 0
+	} else return Math.max.apply(null, array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array == undefined) {
+		return 0
+	} else if (array.length == 0) {
+		return 0
+	} else {
+		let sum = array.reduce((accum, item) => accum + item, 0);
+		let result = sum / array.length;
+		return result
+	}
 }
+
+
+
+
